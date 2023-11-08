aa15b8d70ea8f8653eef5c395ee949ba8df57b6a
Zyrica Drevin <zyrica.drevin@caspeco.se>
2023-11-08 14:35:25 +0100

    app-picker.js

--- app-picker/src/index.js
+++ app-picker/src/index.js
@@ -22,8 +22,16 @@ customElements.define(
             template.querySelector('.name').innerHTML = this.getAttribute('name');
             this.appendChild(template);
 
+            const close = () => {
+                document.getElementById('app-picker-modal').classList.remove('open');
+                document.removeEventListener('click', close);
+            };
+
             this.addEventListener('click', () => {
                 document.getElementById('app-picker-modal').classList.toggle('open');
+                setTimeout(() => {
+                    document.addEventListener('click', close);
+                });
             });
         }
     }
@@ -38,21 +46,21 @@ customElements.define(
             template.querySelector('.abbreviation').innerHTML = this.getAttribute('abbreviation');
             template.querySelector('.name').innerHTML = this.getAttribute('name');
             template.querySelector('.description').innerHTML = this.getAttribute('description');
+            template.querySelector('a').setAttribute('href', this.getAttribute('url'));
             this.appendChild(template);
-
-            this.addEventListener('click', () => {
-                document.getElementById('app-picker-modal').classList.toggle('open');
-                window.location = this.getAttribute('url');
-                mount();
-            });
         }
     }
 );
 
+let first = true;
 let count = 0;
 const mount = () => {
+
+    document.body.classList.remove('admin', 'personal', 'kassa', 'bokning', 'analys');
+
     const container = document.querySelector(".framework_menu_container") || document.querySelector('.menu-container');
-    if (!container) {
+    const systemSelect = document.getElementById('main.selectSystem.dropdownListView');
+    if (!container || !systemSelect) {
         if (count++ < 300) setTimeout(mount, 100);
         return;
     }
@@ -62,9 +70,9 @@ const mount = () => {
     const item = document.createElement('div');
     container.insertBefore(item, container.firstChild);
 
-    let color = '#4530C9';
-    let abbreviation = 'Pe';
-    let name = 'Personal';
+    let color = "#0c0656";
+    let abbreviation = "Ad";
+    let name = "Admin";
     const hash = window.location.hash.toLowerCase();
     const isCheckout = document.querySelector('.menu-container');
     if (isCheckout) {
@@ -79,6 +87,10 @@ const mount = () => {
         color = "#EE657A";
         abbreviation = "An";
         name = "Analys";
+    } else if (hash.includes('personal')) {
+        color = '#4530C9';
+        abbreviation = 'Pe';
+        name = 'Personal';
     }
 
     item.outerHTML = `
@@ -88,6 +100,31 @@ const mount = () => {
         name="${name}"
     ></app-picker-menu-item>
     `;
+
+    if (first && !isCheckout) {
+        first = false;
+        if (isCheckout) {
+
+        } else {
+            document.getElementById('menu.checkout.text').innerHTML = 'Artikelregister';
+            document.body.classList.add(name.toLowerCase());
+
+            const sideBar = document.querySelector('.framework_sidebar');
+            sideBar.style['padding-bottom'] = '16px';
+            const navbarFooter = document.createElement('div');
+            sideBar.appendChild(navbarFooter);
+
+            const help = document.getElementById('menu.support');
+            const me = document.getElementById('menu.me');
+            const logout = document.getElementById('menu.logout');
+
+            navbarFooter.appendChild(help);
+            navbarFooter.appendChild(me);
+            navbarFooter.appendChild(logout);
+
+            container.insertBefore(systemSelect, container.firstChild);
+        }
+    }
 }
 
 mount();
